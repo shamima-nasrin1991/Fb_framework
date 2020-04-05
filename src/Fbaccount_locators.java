43a667c60bed92345c9f973029f469d0c09304c9
shamima-nasrin1991 <shamimas102@gmail.com>
2020-04-04 23:16:21 -0400

    In this(locators) file i used different locators(xpath,id,css selector) to identify web element. to create Fb account.

---
+++ src/Fbaccount_locators.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.locators;
+
+/**
+ * @author Imtiaz
+ *
+ */
+public class Fbaccount_locators {
+	//this locator value is for First name  as using  xpath 
+	public String fb_firstname_xpath ="//input[@id='u_0_m']";
+	//this locator value is for First name  as using  id
+	public String fb_firstname_id ="u_0_m";
+	//this locator value is for First name  as using cssSelector
+	public String fb_firstname_css ="#u_0_m";
+	//this locator value is for Last name  as  using xpath 
+	public String fb_lastname_xpath = "//input[@id='u_0_o']";
+	//this locator value is for Last name  as  using cssSelector
+	public String fb_lastname_css = "#u_0_o";
+	//this locator value is for Last name  as using id
+	public String fb_lastname_id = "u_0_o";
+	//this locator value is for email ,password,click as  using xpath 
+public String fb_email_locator = "//input[@id='u_0_r']";
+public String fb_password_locator = "//input[@id='u_0_w']";
+public String fb_click_locator =  "//button[@id='u_0_13']";
+//this locator value is for  email ,password,click as  using cssSelector
+public String fb_email_csslocator ="#u_0_r";
+public String fb_password_csslocator ="#u_0_w";
+public String fb_click_csslocator = "#u_0_13";
+//this locator value is for email ,password,click as  using id
+public String fb_email_id ="u_0_r";
+public String fb_password_id ="u_0_w";
+public String fb_click_id= "u_0_13";
+//This locator value is for Re-enter email as using xpath, css, id
+public String fb_Remail_xpath = "//input[@id='u_0_u']";
+public String fb_Remail_css = "#u_0_u";
+public String fb_Remail_id = "u_0_u";
+//this locator value is for drop down list( Month/Date of birth) using   as using xpath,css,id
+public String fb_month_xpath ="//select[@id='month']"; 
+public  String fb_month_css = "#month";
+public  String fb_month_id = "month";
+//this locator value is for drop down list( Date /Date of birth) using   as using xpath,css,id
+public String fb_day_xpath = "//select[@id='day']";
+public String fb_day_css = "#day";
+public String fb_day_id = "day";
+//this locator value is for drop down list(year /Date of birth) using   as using xpath,css,id
+public String fb_year_xpath ="//select[@id='year']"; 
+public String fb_year_css = "#year";
+public String fb_year_id = "year";
+//this locator value is for Radio button using as By.name() locator
+public String fb_radio_name = "sex";
+public String fb_radio_M_id = "u_0_7";
+public String fb_radio_M_xpath = "//input[@id='u_0_7']";
+public String fb_radio_F_id = "u_0_6";
+public String fb_radio_F_xpath = "//input[@id='u_0_6']";
+}
