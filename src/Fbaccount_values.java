b81d43425be673248691fdd144f910b087985131
shamima-nasrin1991 <shamimas102@gmail.com>
2020-04-04 23:28:23 -0400

    In this file have all the locators values

---
+++ src/Fbaccount_values.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.values;
+
+/**
+ * @author Imtiaz
+ *
+ */
+public class Fbaccount_values {
+	public String fb_firstname_values = "Shamima";
+	public String fb_lastname_values = "Nasrin";
+	public String fb_email_values = "Shamimas102@gmail.com";
+	public String fb_reemail_values = "Shamimas102@gmail.com";
+	public String fb_password_values = "Snasrin?1991";
+	public String fb_bdmonth_values ="7";
+	public String fb_bdday_values ="1"; 
+	public String fb_bdyear_values = "1991";
+
+}
